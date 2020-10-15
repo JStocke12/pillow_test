24d169880f2216fca8f1e53bbd88f961d88b2fb1
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2020-10-15 15:19:49 -0400

    Rendering calibration circle

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from PIL import Image
 
-img = Image.new('RGB', (256, 144))
+width = 256
+height = 144
 
-print(img.load()[4,4])
+img = Image.new('RGB', (width, height))
+
+px = img.load()
+
+for i in range(width):
+    for j in range(height):
+        px[i,j] = (0, 0, 0) if (height/2-j)**2+(i-width/2)**2>4096 or (height/2-j)**2+(i-width/2)**2<3136 else (255, 255, 255)
 
 img.save('image.png')
